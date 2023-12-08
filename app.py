85a96830ff0818db44c2fa8b828c505c18418812
B KASHI VISHWANATHA REDDY <139041125+kashi256@users.noreply.github.com>
2023-12-08 20:08:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template, request
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+
+import numpy as np
+
+app = Flask(__name__)
+dic = {0 : 'NO-CANCER', 1 : 'CANCER'}
+model = load_model('cancer_detection_model.h5')
+model.make_predict_function()
+def predict_label(img_path):
+	i = image.load_img(img_path, target_size=(100,100))
+	i = image.img_to_array(i)/255.0
+	i = i.reshape(1, 100,100,3)	
+                  #p = model.predict_classes(i)
+	p = np.argmax(model.predict(i), axis=-1)
+	return dic[p[0]]
+
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("index.html")
+
+@app.route("/about")
+def about_page():
+	return "KASHI REDDY..!!!"
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	if request.method == 'POST':
+		img = request.files['my_image']
+		img_path = "static/" + img.filename	
+		img.save(img_path)
+		p = predict_label(img_path)
+	return render_template("index.html", prediction = p, img_path = img_path)
+
+if __name__ =='__main__':
+	#app.debug = True
+	app.run(debug = True)
